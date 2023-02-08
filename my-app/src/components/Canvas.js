1b11679398d7e3727d8101865d76806aee474a87
smilehyeon <ausp94@naver.com>
2023-02-08 17:45:37 +0900

    3

--- my-app/src/components/Canvas.js
+++ my-app/src/components/Canvas.js
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState, forwardRef } from 'react';
 import bgImg from '../components/assets/background.png';
 import styles from "./Canvas.module.css";
 
-function Canvas ({values}) {
+function Canvas({ values, btnClick }) {
     const image = new Image();
     const image2 = new Image();
     const image3 = new Image();
@@ -14,77 +14,81 @@ function Canvas ({values}) {
     const canvasRef2 = useRef();
     const canvasRef3 = useRef();
     const canvasRef4 = useRef();
+    const canvas = canvasRef?.current;
+    const context = canvas?.getContext('2d');
+    const canvas2 = canvasRef2?.current;
+    const context2 = canvas2?.getContext('2d');
+    const canvas3 = canvasRef3?.current;
+    const context3 = canvas3?.getContext('2d');
+    const canvas4 = canvasRef4?.current;
+    const context4 = canvas4?.getContext('2d');
+
+    const file1 = useRef();
 
     useEffect(() => {
         drawCanvas();
     });
 
     function drawCanvas() {
-        const canvas = canvasRef?.current;
-        const context = canvas?.getContext('2d');
-        const canvas2 = canvasRef2?.current;
-        const context2 = canvas2?.getContext('2d');
-        const canvas3 = canvasRef3?.current;
-        const context3 = canvas3?.getContext('2d');
-        const canvas4 = canvasRef4?.current;
-        const context4 = canvas4?.getContext('2d');
 
-        image.src=values.value1;
-        image2.src=values.value2;
-        image3.src=values.value3;
-        image4.src=values.value4;
+        const {value1, value2, value3, value4} = values;
+        image.src = value1;
+        image2.src = value2;
+        image3.src = value3;
+        image4.src = value4;
 
 
-        image.onload = function(){
-            context.drawImage(image,0,0,500,500);
-        } 
-        image2.onload = function(){
-            context2.drawImage(image2,0,0,500,500);
-        } 
-        image3.onload = function(){
-            context3.drawImage(image3,0,0,500,500);
-        } 
-        image4.onload = function(){
-            context4.drawImage(image4,0,0,500,500);
-        } 
 
-    } 
+        image.onload = function () {
+            context.drawImage(image, 0, 0, context.canvas.width, context.canvas.height);
+        }
+        image2.onload = function () {
+            context2.drawImage(image2, 0, 0, context.canvas.width, context.canvas.height);
+        }
+        image3.onload = function () {
+            context3.drawImage(image3, 0, 0, context.canvas.width, context.canvas.height);
+        }
+        image4.onload = function () {
+            context4.drawImage(image4, 0, 0, context.canvas.width, context.canvas.height);
+        }   
+    }
+
+    
+    function onImgClick(e) {
+        var name = e.target.getAttribute("name");
+        btnClick(name);
+    }
 
     return (
-            
-                <div className={styles.photos}>
-                    <div id="image1" className={styles.photo_frame}>
-                    <canvas ref={canvasRef} name="value1"></canvas>
-                        <span id="desc1" className={styles.photo_description}>
-                            첫 번째 이미지 설명
-                        </span>
-                    </div>
-                    <div id="image2" className={styles.photo_frame}>
-                    <canvas ref={canvasRef2} name="value2"></canvas>
-                        <span id="desc2" className={styles.photo_description}>
-                            두 번째 이미지 설명
-                        </span>
-                    </div>
-                    <div id="image3" className={styles.photo_frame}>
-                    <canvas ref={canvasRef3} name="value3"></canvas>
-                        <span id="desc3" className={styles.photo_description}>
-                            세 번째 이미지 설명
-                        </span>
-                    </div>
-                    <div id="image4" className={styles.photo_frame}>
-                    <canvas ref={canvasRef4} name="value4"></canvas>
-                        <span id="desc4" className={styles.photo_description}>
-                            네 번째 이미지 설명
-                        </span>
-                    </div>
-                
-                {/* <canvas ref={canvasRef} name="value1"></canvas>
+
+        <div className={styles.photos}>
+            <div id="image1" name="i11" className={styles.photo_frame} onClick={onImgClick}>
+                <canvas ref={canvasRef} name="value1"></canvas>
+                <span id="desc1" className={styles.photo_description}>
+                    귀엽다🤟
+                </span>
+            </div>
+            <div id="image2" className={styles.photo_frame} onClick={onImgClick}>
                 <canvas ref={canvasRef2} name="value2"></canvas>
+                <span id="desc2" className={styles.photo_description}>
+                    사랑스럽다😍
+                </span>
+            </div>
+            <div id="image3" className={styles.photo_frame} onClick={onImgClick}>
                 <canvas ref={canvasRef3} name="value3"></canvas>
-                <canvas ref={canvasRef4} name="value4"></canvas> */}
-                </div>
-            
-       
+                <span id="desc3" className={styles.photo_description}>
+                    멋지다😎
+                </span>
+            </div>
+            <div id="image4" className={styles.photo_frame} onClick={onImgClick}>
+                <canvas ref={canvasRef4} name="value4"></canvas>
+                <span id="desc4" className={styles.photo_description}>
+                💖 그게 바로 나 💖
+                </span>
+            </div>
+        </div>
+
+
     );
 }
 
--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useEffect, useRef, useState, forwardRef } from 'react';
 import Canvas from "../components/Canvas";
 import styles from "./Home.module.css";
+import html2canvas from "html2canvas"
 
 
 function Home() {
@@ -16,6 +17,10 @@ function Home() {
 
     // const [value, setValue] = useState();
     const { value1, value2, value3, value4 } = values;
+    const file1 = useRef("");
+    const file2 = useRef("");
+    const file3 = useRef("");
+    const file4 = useRef("");
 
     function onChange(e) {
         const { value, name } = { value: URL.createObjectURL(e.target.files[0]), name: e.target.name }
@@ -26,25 +31,60 @@ function Home() {
         })
     }
 
+
+    // 화면 캡처
+    const onCapture = () => {
+        console.log("캡처!");
+        html2canvas(document.getElementById('container2')).then(canvas => {
+            onSave(canvas.toDataURL('image/png'), 'image-downloda.png')
+        });
+    };
+
+    // 캡처 이미지 저장
+    const onSave = (url, fileName) => {
+        console.log('저장');
+        var link = document.createElement('a');
+        document.body.appendChild(link);
+        link.href = url;
+        link.download = fileName;
+        link.click();
+        document.body.removeChild(link);
+
+    }
+
+
+    const onClick = (name) => {
+        switch (name) {
+            case "value1" : file1.current.click(); break;
+            case "value2" : file2.current.click(); break;
+            case "value3" : file3.current.click(); break;
+            case "value4" : file4.current.click(); break;
+        }
+    }
+
+
     return (
 
         <div className={styles.container}>
-            <div className={styles.container2}>
-                <Canvas values={values} />
+            <div id="container2" className={styles.container2}>
+                <Canvas values={values} btnClick={onClick}/>
 
                 <div className={styles.footer}>
                     <p className={styles.f_title}>Happy Lunch Time</p>
-                    <p className={styles.f_date}>2023.01.23</p>
+                    <p className={styles.f_date}>며네네컷</p>
                 </div>
 
             </div>
 
 
             <div className={styles.inpImg}>
-                <input name="value1" accept="image/*" type='file' id="imgInp" onChange={onChange} />
-                <input name="value2" accept="image/*" type='file' id="imgInp2" onChange={onChange} />
-                <input name="value3" accept="image/*" type='file' id="imgInp3" onChange={onChange} />
-                <input name="value4" accept="image/*" type='file' id="imgInp4" onChange={onChange} />
+                <input name="value1" accept="image/*" type='file' id="imgInp" onChange={onChange} ref={file1}/>
+                <input name="value2" accept="image/*" type='file' id="imgInp2" onChange={onChange} ref={file2}/>
+                <input name="value3" accept="image/*" type='file' id="imgInp3" onChange={onChange} ref={file3}/>
+                <input name="value4" accept="image/*" type='file' id="imgInp4" onChange={onChange} ref={file4}/>
+            </div>
+            <div className={styles.download}>
+                <button className={styles.btn} onClick={onCapture}>download</button>
             </div>
         </div>
     );
